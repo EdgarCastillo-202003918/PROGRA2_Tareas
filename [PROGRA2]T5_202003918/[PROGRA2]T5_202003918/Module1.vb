6da3cab73a5eda9c0cf31eae23f1218788d35eff
EdgarCastillo-202003918 <110765870+EdgarCastillo-202003918@users.noreply.github.com>
2022-08-30 00:11:44 -0600

    Tarea 5

---
+++ [PROGRA2]T5_202003918/[PROGRA2]T5_202003918/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim acum, i, indice, num, contador As Integer
+        Console.Write("Ingrese numero: ")
+        num = Console.ReadLine()
+        acum = 1
+
+        For i = 1 To num
+            acum = acum * i
+        Next
+        Console.WriteLine("Factorial: " & acum)
+        Console.WriteLine("")
+        Console.WriteLine("Multiplos de 4:")
+
+
+        indice = -1
+        contador = 0
+        While (indice <= acum)
+            indice = indice + 1
+            If Not (indice Mod 4 = 0) Then
+                Continue While
+            Else
+                contador = contador + 1
+            End If
+            Console.WriteLine(indice)
+        End While
+
+        Console.WriteLine("Hay " & contador.ToString & " multiplos de 4")
+
+        Console.ReadLine()
+    End Sub
+
+End Module
